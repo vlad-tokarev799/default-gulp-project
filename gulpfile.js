ce11c90b0541d3275cd30e315d74dc710e671500
vlad-tokarev799 <vlad.tokarev.200247@gmail.com>
2022-04-17 17:34:27 +0800

    added server task

--- gulpfile.js
+++ gulpfile.js
@@ -1,215 +1,214 @@
-'use strict'
-const {src, dest} = require('gulp'); 
-const gulp = require('gulp');
-const browsersync = require('browser-sync').create();
-const del = require('del');
-const cleancss = require('gulp-clean-css');
-const imagemin= require('gulp-imagemin');
-const plumber = require('gulp-plumber');
-const rename = require('gulp-rename');
-const rigger = require('gulp-rigger');
-const sass = require("gulp-sass")(require('sass'));
-const removecomments = require("gulp-strip-css-comments");
-const uglify = require("gulp-uglify");
-const panini = require("panini");
-const autoprefixer = require('gulp-autoprefixer');
-const gcmq = require('gulp-group-css-media-queries');
-const htmlmin = require('gulp-htmlmin');
-const pug = require('gulp-pug');
-const imgCompress = require('imagemin-jpeg-recompress')
+const {src, dest, parallel, task} = require('gulp'),
+	gulp = require('gulp'),
+	browsersync = require('browser-sync').create(),
+	del = require('del'),
+	cleancss = require('gulp-clean-css'),
+	plumber = require('gulp-plumber'),
+	rigger = require('gulp-rigger'),
+	sass = require('gulp-sass')(require('sass')),
+	removecomments = require('gulp-strip-css-comments'),
+	uglify = require('gulp-uglify'),
+	autoprefixer = require('gulp-autoprefixer'),
+	gcmq = require('gulp-group-css-media-queries'),
+	htmlmin = require('gulp-htmlmin'),
+	pug = require('gulp-pug'),
+	gulpif = require('gulp-if')
 
-var path = {
-    build: {
-        html: 'dist/',
-        js: 'dist/assets/js/',
-        css: 'dist/assets/css/',
-        images: 'dist/assets/img/',
-        fonts: 'dist/assets/fonts/'
-    },
-    prod: {
-        html: 'prod/',
-        js: 'prod/assets/js/',
-        css: 'prod/assets/css/',
-        images: 'prod/assets/img/',
-        fonts: 'prod/assets/fonts/'
-    },
-    src: {
-        html: "src/*.html",
-        js: "src/assets/js/*.js",
-        css: "src/assets/sass/style.+(scss|sass)",
-        images: "src/assets/img/**/*.{jpg,png,svg,gif,ico}",
-        fonts: "src/assets/fonts/**/*.*",
-        pug: "src/*.pug",
-    },
-    watch: {
-        html: "src/**/*.html",
-        js: "src/assets/js/**/*.js",
-        css: "src/assets/sass/**/*.+(scss|sass)",
-        images: "src/assets/img/**/*.{jpg,png,svg,gif,ico}",
-        fonts: "src/assets/fonts/**/*.*",
-        pug: "src/**/*.pug",
-        pugComponents: "src/assets/components/*.pug"
-    },
-    cleanDist: "./dist",
-    cleanProd: "./prod"
+const baseDirs = {
+	default: 'dist',
+	build: 'build',
+	server: 'server'
 }
 
+function getDirs(dir) {
+	return {
+		base: `${dir}/`,
+		js: `${dir}/assets/js`,
+		css: `${dir}/assets/css/`,
+		images: `${dir}/assets/img/`,
+		fonts: `${dir}/assets/fonts/`
+	}
+}
 
+const path = {
+	default: getDirs(baseDirs.default),
+	build: getDirs(baseDirs.build),
+	server: getDirs(baseDirs.server),
+	src: {
+		html: 'src/**/*{html,php,pug}',
+		js: 'src/assets/js/*.js',
+		css: 'src/assets/sass/style.{scss,sass}',
+		images: 'src/assets/img**/*.{jpg,png,svg,gif,ico,webp,jpeg}',
+		fonts: 'src/assets/fonts/**/*.*',
+		data: 'src/**/*.{json,txt,md,htaccess}'
+	},
+	watch: {
+		html: "src/**/*.{html,php,pug}",
+		js: 'src/assets/js/**/*.js',
+		css: "src/assets/sass/**/*.{scss,sass,css}",
+		images: 'src/assets/img/**/*.{jpg,png,svg,gif,ico,webp,jpeg}',
+		fonts: 'src/assets/fonts/**/*.*',
+		data: 'src/**/*.{json,txt,md,htaccess}'
+	}
+}
 
 function browserSync(done) {
-    browsersync.init({
-        server: {
-            baseDir: "./dist/"
-        },
-        port: 3000,
-        notify: false
-    });
-}
-function browserSyncReload(done) {
-    browsersync.reload();
+	browsersync.init({
+		server: {
+			baseDir: "./dist"
+		},
+		port: 3000,
+		notify: false
+	})
 }
 
-// dev build
-
-function html() {
-    return src(path.src.html, { base: "src/"})
-        .pipe(plumber())
-        .pipe(dest(path.build.html))
-        .pipe(browsersync.stream());
+const check = {
+	notPHP: file => (file.extname != '.php'),
+	isPug: file => (file.extname == '.pug')
 }
 
-function pugTask() {
-    return src(path.src.pug, {base: 'src/'})
-        .pipe(plumber())
-        .pipe(pug())
-        .pipe(dest(path.build.html))
-        .pipe(browsersync.stream())
+const dev = {
+	html: () => {
+		return src(path.src.html)
+			.pipe(plumber())
+			.pipe(gulpif(check.isPug, pug()))
+			.pipe(gulpif(check.notPHP, dest(path.default.base)))
+			.pipe(browsersync.stream())
+	},
+	css: () => {
+		return src(path.src.css)
+			.pipe(plumber())
+			.pipe(sass({outputStyle: 'expanded'}))
+			.pipe(dest(path.default.css))
+			.pipe(browsersync.stream())
+	},
+	js: () => {
+		return src(path.src.css)
+			.pipe(plumber())
+			.pipe(rigger())
+			.pipe(dest(path.default.js))
+			.pipe(browsersync.stream())
+	},
+	images: () => {
+		return src(path.src.images)
+			.pipe(dest(path.default.images))
+			.pipe(browsersync.stream())
+	},
+	fonts: () => {
+		return src(path.src.fonts)
+			.pipe(gulp.dest(path.default.fonts))
+	},
+	watch: () => {
+		gulp.watch([path.watch.html], this.html)
+		gulp.watch([path.watch.js], this.js)
+		gulp.watch([path.watch.css], this.css)
+		gulp.watch([path.watch.images], this.images)
+		gulp.watch([path.watch.fonts], this.fonts)
+	},
+	clear: () => {
+		return del(path.default.base)
+	}
 }
 
-function css() {
-    return src(path.src.css, { base: "src/assets/sass/"})
-    .pipe(plumber())
-    .pipe(sass({outputStyle: 'expanded'}))
-    .pipe(dest(path.build.css))
-    .pipe(browsersync.stream());
+const build = {
+	html: () => {
+		return src(path.src.html)
+			.pipe(plumber())
+			.pipe(gulpif(check.isPug, pug()))
+			.pipe(gulpif(check.notPHP, htmlmin({
+				collapseWhitespace: true,
+				removeComments: true
+			})))
+			.pipe(dest(path.build.base))
+	},
+	css: () => {
+		return src(path.src.css)
+			.pipe(plumber())
+			.pipe(sass({outputStyle: 'compressed'}))
+			.pipe(autoprefixer({
+				overrideBrowserslist: ['last 15 versions', '> 1%', 'ie 8', 'ie 7'],
+				cascade: true
+			}))
+			.pipe(gcmq())
+			.pipe(cleancss())
+			.pipe(removecomments())
+			.pipe(dest(path.build.css))
+	},
+	js: () => {
+		return src(path.src.css)
+			.pipe(plumber())
+			.pipe(rigger())
+			.pipe(uglify())
+			.pipe(dest(path.build.js))
+	},
+	images: () => {
+		return src(path.src.images)
+			.pipe(dest(path.build.images))
+	},
+	data: () => {
+		return src(path.src.data)
+			.pipe(dest(path.build.base))
+	},
+	fonts: () => {
+		return src(path.src.fonts)
+			.pipe(gulp.dest(path.build.fonts))
+	},
+	clear: () => {
+		return del(path.build.base)
+	}
 }
 
-function js() {
-    return src(path.src.js, {base: "./src/assets/js/"})
-        .pipe(plumber())
-        .pipe(rigger())
-        .pipe(dest(path.build.js))
-        .pipe(browsersync.stream());
+const server = {
+	html: () => {
+		return src(path.src.html)
+			.pipe(plumber())
+			.pipe(gulpif(check.isPug, pug()))
+			.pipe(dest(path.server.base))
+	},
+	css: () => {
+		return src(path.src.css)
+			.pipe(plumber())
+			.pipe(sass({outputStyle: 'expanded'}))
+			.pipe(dest(path.server.css))
+	},
+	js: () => {
+		return src(path.src.js)
+			.pipe(plumber())
+			.pipe(rigger())
+			.pipe(dest(path.server.js))
+	},
+	images: () => {
+		return src(path.src.images)
+			.pipe(dest(path.server.images))
+	},
+	fonts: () => {
+		return src(path.src.fonts)
+			.pipe(dest(path.server.fonts))
+	},
+	data: () => {
+		return src(path.src.data)
+			.pipe(dest(path.server.base))
+	},
+	clear: () => {
+		return del(path.server.base)
+	},
+	watch: () => {
+		gulp.watch([path.watch.html], this.html)
+		gulp.watch([path.watch.js], this.js)
+		gulp.watch([path.watch.css], this.css)
+		gulp.watch([path.watch.images], this.images)
+		gulp.watch([path.watch.fonts], this.fonts)
+		gulp.watch([path.watch.data], this.data)
+	}
 }
 
-function images() {
-    return src(path.src.images)
-        .pipe(dest(path.build.images))
-        .pipe(browsersync.stream());
+const main = gulp.series(dev.clear, gulp.parallel([dev.html, dev.css, dev.js, dev.images, dev.fonts]))
+const prod = gulp.parallel([build.html, build.css, build.js, build.images, build.fonts, build.data])
+const serv = gulp.series(server.clear, gulp.parallel(server.html, server.css, server.js, server.images, server.fonts, server.data), server.watch)
+
+
+module.exports = {
+	default: gulp.parallel(main, dev.watch, browserSync), 
+	build: gulp.series(build.clear, prod),
+	server: serv
 }
-
-function fonts() {
-    return src(path.src.fonts)
-        .pipe(gulp.dest(path.build.fonts));
-}
-
-function cleanDist() {
-    return del(path.cleanDist);
-}
-function cleanProd() {
-    return del(path.cleanProd);
-}
-
-function watchFiles() {
-    gulp.watch([path.watch.html], html);
-    gulp.watch([path.watch.css], css);
-    gulp.watch([path.watch.js], js);
-    gulp.watch([path.watch.images], images);
-    gulp.watch([path.watch.fonts], fonts);
-    gulp.watch([path.watch.pug], pugTask);
-    gulp.watch([path.watch.pugComponents], pugTask);
-}
-
-// prod build
-
-function prodHtml() {
-    return src(path.src.html, { base: "src/"})
-        .pipe(plumber())
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-            removeComments: true
-        }))
-        .pipe(dest(path.prod.html));
-}
-
-function prodPug() {
-    return src(path.src.pug, {base: 'src/'})
-        .pipe(plumber())
-        .pipe(pug())
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-            removeComments: true
-        }))
-        .pipe(dest(path.prod.html))
-}
-
-function prodCss() {
-    return src(path.src.css, { base: "src/assets/sass/"})
-    .pipe(plumber())
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(autoprefixer({
-        overrideBrowserslist: ['last 15 versions', '> 1%', 'ie 8', 'ie 7'],
-        cascade: true
-    }))
-    .pipe(gcmq())
-    .pipe(cleancss())
-    .pipe(removecomments())
-    .pipe(dest(path.prod.css));
-}
-
-function prodJs() {
-    return src(path.src.js, {base: "./src/assets/js/"})
-        .pipe(plumber())
-        .pipe(rigger())
-        .pipe(uglify())
-        .pipe(dest(path.prod.js));
-}
-
-function prodImages() {
-    return src(path.src.images)
-        .pipe(imagemin([
-            imgCompress({
-              loops: 4,
-              min: 70,
-              max: 80,
-              quality: 'high'
-            }),
-            imagemin.gifsicle(),
-            imagemin.optipng(),
-            imagemin.svgo()
-          ]))
-        .pipe(dest(path.prod.images));
-}
-
-function prodFonts() {
-    return src(path.src.fonts)
-        .pipe(gulp.dest(path.prod.fonts));
-}
-
-const prod = gulp.series(cleanProd, gulp.parallel(prodPug, prodHtml, prodCss, prodJs, prodImages, prodFonts));
-const build = gulp.series(cleanDist, gulp.parallel(pugTask, html, css, js, images, fonts));
-const watch = gulp.parallel(build, watchFiles, browserSync);
-
-
-
-exports.html = html;
-exports.css = css;
-exports.js = js;
-exports.images = images;
-exports.fonts = fonts;
-exports.cleanDist = cleanDist;
-exports.cleanProd = cleanProd;
-exports.build = build;
-exports.watch = watch;
-exports.default = watch;
-exports.prod = prod;
